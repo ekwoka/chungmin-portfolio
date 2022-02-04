d7fe7ce36717bbbffed53e213ad863298c7c9678
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-02-04 18:22:08 +0800

    :sparkles: New Hero message

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -6,15 +6,9 @@ export default function Hero() {
             <img src={ heroImage } class="w-full opacity-80 min-h-screen object-cover" alt="AC Hotel Main Lobby" />
             <hero-content class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-y-4">
                 <h2 class="text-3xl tracking-wider block">
-                    Interior Designer
-                </h2>
-                <h2 class="text-3xl tracking-wider block">
-                    Hospitality
-                </h2>
-                <h2 class="text-3xl tracking-wider block">
-                    Sustainability
+                    Senior Interior Designer with focus on Hospitality and Sustainable Design
                 </h2>
             </hero-content>
         </section>
     )
-}
+}in
