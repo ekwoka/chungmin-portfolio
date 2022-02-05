ea2c004982581ff7fc5eb8a18be0cd820ec87e4a
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-02-05 18:20:27 +0800

    :sparkles: new mobile menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import { Link } from 'preact-router/match';
 import { useEffect, useState } from 'preact/hooks';
+import MobileNav from '../mobileMenu';
 
 const NAVIGATION = [
   {
@@ -14,6 +15,7 @@ const NAVIGATION = [
 
 const Header = () => {
   const [isScrolled, setScroll] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -22,18 +24,22 @@ const Header = () => {
   }, []);
 
   return (
-    <header class={`backdrop-blur py-4 px-8 fixed top-0 inset-x-0 transition-all duration-500 z-10 ${isScrolled ? 'bg-stone-50 text-stone-900 shadow' : 'bg-stone-50/0 text-stone-900 shadow-none'}`}>
-      <header-wrapper class="flex flex-row justify-between gap-4 max-w-screen-md mx-auto">
-        <h1 class="text-xl flex-shrink-0">Claire Lee</h1>
-        <nav class="flex gap-4 flex-shrink">
-          {NAVIGATION.map((item, i) => (
-            <Link key={i} href={item.target} class="block transition-all hover:tracking-widest min-w-[8ch] text-center drop-shadow" activeClass="font-bold pointer-events-none">
-              {item.label}
-            </Link>
-          ))}
-        </nav>
-      </header-wrapper>
-    </header>
+    <>
+      <header class={`backdrop-blur-sm py-4 px-8 fixed top-0 inset-x-0 transition-all duration-500 z-20 ${isScrolled || navOpen ? 'bg-stone-50 text-stone-900 shadow' : 'bg-stone-50/0 text-stone-900 shadow-none'}`}>
+        <header-wrapper class="flex flex-row justify-between mx-auto gap-4 max-w-screen-md">
+          <h1 class="flex-shrink-0 px-4 py-2 text-xl">Claire Lee</h1>
+          <nav class="flex-shrink hidden gap-4 md:flex">
+            {NAVIGATION.map((item, i) => (
+              <Link key={i} href={item.target} class="block transition-all hover:tracking-widest min-w-[8ch] text-center drop-shadow py-2 px-4" activeClass="font-bold pointer-events-none">
+                {item.label}
+              </Link>
+            ))}
+          </nav>
+          <MobileNav className="block md:hidden" navigation={NAVIGATION} setOpen={setNavOpen} />
+        </header-wrapper>
+      </header>
+      <div class={`fixed inset-0 w-full h-screen z-10 bg-stone-900 transition-opacity ${navOpen ? 'opacity-75 duration-700' : 'opacity-0 duration-300'}`}></div>
+    </>
   );
 };
 
---
+++ src/components/mobileMenu/index.js
@@ -0,0 +1,59 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+import { CheckCircleIcon, MenuIcon } from '@heroicons/react/outline';
+import { useEffect } from 'preact/hooks';
+import { Link } from 'preact-router/match';
+
+const callsToAction = [
+  { name: 'Contact', href: '#'},
+  { name: 'View CV', href: '#'},
+  { name: 'LinkedIn', href: '#'}
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function MobileNav({ className, setOpen, navigation }) {
+  return (
+    <Popover class="relative">
+      {({ open, close }) => {
+        useEffect(() => setOpen(open), [open]);
+        return (
+          <>
+            <div class={className}>
+              <Popover.Button class={classNames(open ? 'text-stone-500' : 'text-stone-900', 'py-2 px-4 inline-flex items-center')}>
+                <MenuIcon class="w-6 h-6" />
+              </Popover.Button>
+            </div>
+
+            <Transition as={Fragment} enter="transition duration-500" enterFrom="opacity-0 translate-x-2" enterTo="opacity-100 translate-x-0" leave="transition duration-500" leaveFrom="opacity-100 translate-x-0" leaveTo="opacity-0 translate-x-2">
+              <Popover.Panel class="fixed inset-x-0 z-10 shadow-lg transform mt-5">
+                <div class="bg-stone-50 text-stone-900">
+                  <div class="px-4 pb-6 mx-auto flex flex-col gap-y-2">
+                    {navigation.map((item) => (
+                      <Link key={item.label} href={item.target} class="block transition-all hover:tracking-widest min-w-[8ch] text-center py-2 px-4 text-2xl" activeClass="font-bold pointer-events-none" onClick={close}>
+                      {item.label}
+                    </Link>
+                    ))}
+                  </div>
+                </div>
+                <div class="bg-stone-100 border-t border-stone-200">
+                  <div class="px-4 py-5 mx-auto flex flex-col gap-2 max-w-max">
+                    {callsToAction.map((item) => (
+                        <Link href={item.href} class="flex items-center text-base font-medium text-gray-900 transition duration-150 ease-in-out rounded-md hover:bg-gray-100">
+                          <CheckCircleIcon class="flex-shrink-0 w-6 h-6 text-gray-400" aria-hidden="true" />
+                          <span class="ml-3">{item.name}</span>
+                        </Link>
+                    ))}
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        );
+      }}
+    </Popover>
+  );
+}
