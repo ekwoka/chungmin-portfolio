31cab6dd6feba8a9c0e7435358b7bbb85ba2d91e
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-02-05 18:22:24 +0800

    :construction: Adjust Mobile Styling

--- src/components/project/hero/index.js
+++ src/components/project/hero/index.js
@@ -3,8 +3,8 @@ import heroImage from '../../../static/images/uploads/l1-main-lobby-edited-by-we
 export default function ProjectHero() {
   return (
     <project-hero class="w-full max-h-[75vh] relative shadow">
-      <img class="w-full max-h-[75vh] object-cover" src={heroImage} />
-      <project-info class="absolute bottom-0 max-w-prose mx-auto p-4 bg-stone-50/50 inset-x-0 backdrop-blur">
+      <img class="w-full max-h-[75vh] min-h-[50vh] md:min-h-0 object-cover" src={heroImage} />
+      <project-info class="absolute inset-x-0 bottom-0 p-4 mx-auto max-w-prose bg-stone-50/50 backdrop-blur">
         <h1 class="text-4xl">AC Hotel</h1>
         <h2 class="text-lg">Gangnam, Seoul, South Korea</h2>
         <h2 class="text-lg">Senior Designer, Assistant Design Director</h2>
