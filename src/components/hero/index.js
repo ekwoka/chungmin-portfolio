74dbd6b0c2a03f5068eff0efdbd398ef0d6b08d8
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-02-06 15:00:57 +0800

    :construction: Adjust Hero Layout

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -4,9 +4,9 @@ import heroImage from '../../static/images/uploads/AC-hotel_lobby.jpg'
 
 export default function Hero() {
     return (
-        <section class="relative w-full">
+        <section class="relative flex flex-col items-center w-full">
             <img src={ heroImage } class="object-cover w-full min-h-screen opacity-80" alt="AC Hotel Main Lobby" />
-            <hero-content class="absolute flex flex-col top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-y-4">
+            <hero-content class="absolute flex flex-col w-full px-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-y-4">
                 <h2 class="block text-3xl tracking-wider">
                     Senior Interior Designer with focus on Hospitality and Sustainable Design
                 </h2>
