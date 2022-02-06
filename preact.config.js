be3859282841bc5f6ca8c202f89f84ee6e50bd9f
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-02-06 14:57:00 +0800

    :sparkles: Prerenderer Install

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,8 @@
+const netlifyPlugin = require('preact-cli-plugin-netlify');
 const tailwind = require('./src/plugins');
 
 module.exports = (config, env, helpers) => {
   config = tailwind(config, env, helpers);
+  netlifyPlugin(config);
   return config;
 };
---
+++ prerender-urls.js
@@ -0,0 +1,14 @@
+const { generateFileList } = require('./src/crawler');
+const { join } = require('path');
+const fs = require('fs');
+
+module.exports = () => {
+	const pages = [
+		{
+			url: '/',
+			seo: {}
+		}
+	];
+
+	return pages;
+};
