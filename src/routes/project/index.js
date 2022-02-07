13589574b736f4c97401ab48ca12f26daa65ddda
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-02-07 11:23:17 +0800

    :sparkles: Start Image Slider

--- src/routes/project/index.js
+++ src/routes/project/index.js
@@ -3,11 +3,17 @@ import Markdown from 'markdown-to-jsx';
 import project from '../../../content/projects/ac-hotel.md';
 
 import ProjectHero from "../../components/project/hero";
+import ImageSlider from "../../components/galleries/imageSlider";
 
 export default function Project() {
   return (
     <main class="flex flex-col gap-4">
       <ProjectHero />
+      <ImageSlider>
+        <img src="https://placekitten.com/800/800" />
+        <img src="https://placekitten.com/800/800" />
+        <img src="https://placekitten.com/800/800" />
+      </ImageSlider>
       <Markdown class="px-8 mx-auto prose" options={{ wrapper: 'article', forceWrapper: true }}>
         {project.replace(/---(.*\n)*---/, '')}
       </Markdown>
