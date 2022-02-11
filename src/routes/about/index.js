ab1b2aacc85595b87e2df53bfa21670993db93cd
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-02-11 16:42:17 +0800

    :art: Adjust About Formatting

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -8,10 +8,13 @@ const [details, body] = getMD(aboutContent);
 export default function About() {
   return (
     <main class="w-full pt-24">
-      <Hero img={details.image} className="max-h-[50vh]" />
+      <Hero
+        img={details.image}
+        className="max-h-[50vh] w-11/12 md:max-w-prose object-contain "
+      />
       {body.length > 5 && (
         <Markdown
-          class="prose prose-neutral mx-auto py-8 px-4"
+          class="prose prose-neutral mx-auto py-8 px-4 max-w-prose"
           options={pageMDOptions}>
           {body}
         </Markdown>
