4a19f441d8c5c26a1da55bf0dd6bf35f5d0fa532
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-02-12 01:18:01 +0800

    :bug: Fix Header Background

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -24,7 +24,7 @@ const Header = () => {
         class={`py-4 px-8 fixed top-0 inset-x-0 transition-all duration-500 z-20 ${
           isScrolled || navOpen
             ? `text-neutral-900 shadow backdrop-blur ${
-                !navOpen ? 'from-neutral-50 to-neutral-50/75' : 'bg-neutral-50'
+                !navOpen ? 'bg-gradient-to-b from-neutral-50 to-neutral-50/75' : 'bg-neutral-50'
               }`
             : 'text-neutral-900 shadow-none backdrop-blur-0 bg-gradient-to-b from-neutral-50 via-neutral-50/75 to-neutral-50/0'
         }`}>
